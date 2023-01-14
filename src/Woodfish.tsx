67bd4a755c557fc58e0a1c4788944066ca2e3dbd
pnutzz-0207 <77202907+pnutzz-0207@users.noreply.github.com>
2023-01-14 15:57:33 +0800

    timer for woodfish

---
+++ src/Woodfish.tsx
@@ -0,0 +1,36 @@
+import { useEffect, useState, useRef } from "react";
+import woodfishSrc from "../src/woodfish.png";
+
+const Woodfish = () => {
+  //const [clicked, setClicked] = useState<boolean>(false);
+  const [counter, setCounter] = useState(0);
+  const [timeLeft, setTimeLeft] = useState(0.1);
+  const [cssStr, setCssStr] = useState("w-96 h-96");
+
+  const timeoutRef = useRef<ReturnType<typeof setTimeout>>();
+  useEffect(() => {
+    console.log("clicked");
+    console.log(timeLeft);
+    timeoutRef.current = setTimeout(() => {
+      if (timeLeft > 0) {
+        setCssStr("w-80 h-80");
+        setTimeLeft(timeLeft - 1);
+      }
+    }, 500);
+    setCssStr("w-96 h-96");
+    return () => {
+      clearTimeout(timeoutRef.current);
+    };
+  }, [counter]);
+
+  return (
+    <img
+      src={woodfishSrc}
+      alt="woodfish"
+      className={cssStr}
+      onClick={() => setCounter(counter + 1)}
+    />
+  );
+};
+
+export default Woodfish;
